5bfdfb456f069948d84049e070b209d0e2303478
tewing2 <46793705+tewing2@users.noreply.github.com>
2019-01-17 12:50:41 -0600

    Create hello.c
    Added "Mechanical Engineering" line to state my major.

---
+++ hello.c
@@ -0,0 +1,17 @@
+/**
+ * Author: Thomas Ewing
+ * Date: 2016/11/02
+ *
+ * A simple hello world program in C
+ *
+ */
+#include<stdlib.h>
+#include<stdio.h>
+
+int main(int argc, char **argv) {
+
+  printf("Thomas Ewing\n");
+  printf("Mechanical Engineering\n");
+
+  return 0;
+}
